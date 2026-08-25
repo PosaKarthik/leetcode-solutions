c2d430fe24e4feab464385bcacf198cb12ff3bce
PosaKarthik <posakarthik16@gmail.com>
2026-08-25 13:15:36 +0530

    Time: 2 ms (99.65%), Space: 44.7 MB (54.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,35 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if(s.length() != t.length()){
-            return false;
-        }
-  
-        HashMap<Character,Integer> hashMap = new HashMap<>();
-	
-	for(int i=0;i<s.length();i++){
-	    hashMap.put(s.charAt(i),hashMap.getOrDefault(s.charAt(i),0)+1);
-	}
-	
-	
-	for(int i=0;i<t.length();i++){
-	    if(!hashMap.containsKey(t.charAt(i))){
-	        return false;
-	    }else{
-            hashMap.put(t.charAt(i),hashMap.get(t.charAt(i))-1);
-        }
-	}
-
-    for(char x : hashMap.keySet()){
-	    int value = hashMap.get(x);
-	    
-	    if(value != 0){
-	        return false;
-	    }
-	}
-
-    return true;
-	
         
+        int[] count = new int[26];
+
+        for(char c : s.toCharArray()){
+            count[c - 'a']++;
+        }
+
+        for(char c : t.toCharArray()){
+            count[c - 'a']--;
+        }
+
+        for(int x : count){
+
+            if(x != 0){
+                return false;
+            }
+
+        }
+
+        return true;
+
     }
 }
