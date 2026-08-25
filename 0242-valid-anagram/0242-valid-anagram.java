417feddc1b9121940f53175a6f71cccf078e23d5
PosaKarthik <posakarthik16@gmail.com>
2026-08-25 12:54:26 +0530

    Time: 19 ms (10.48%), Space: 47 MB (9.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,31 +4,30 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+  
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+	
+	for(int i=0;i<s.length();i++){
+	    hashMap.put(s.charAt(i),hashMap.getOrDefault(s.charAt(i),0)+1);
+	}
+	
+	
+	for(int i=0;i<t.length();i++){
+	    if(hashMap.containsKey(t.charAt(i))){
+	        hashMap.put(t.charAt(i),hashMap.get(t.charAt(i))-1);
+	    }
+	}
 
-        HashMap<Character,Integer> hashMap1 = new HashMap<>();
-        HashMap<Character,Integer> hashMap2 = new HashMap<>();
+    for(char x : hashMap.keySet()){
+	    int value = hashMap.get(x);
+	    
+	    if(value != 0){
+	        return false;
+	    }
+	}
 
-        for(int i=0;i<s.length();i++){
-            hashMap1.put(s.charAt(i),hashMap1.getOrDefault(s.charAt(i),0)+1);
-            hashMap2.put(t.charAt(i),hashMap2.getOrDefault(t.charAt(i),0)+1);
-        }
-
-       if(hashMap1.size() != hashMap2.size()){
-        return false;
-       }
-
-       for(char x : hashMap1.keySet()){
-
-        char key = x;
-        int value = hashMap1.get(key);
-
-        if(!hashMap2.containsKey(key) || hashMap2.get(key) != value){
-            return false;
-        }
-        
-       }
-
-        return true;
+    return true;
+	
         
     }
 }
