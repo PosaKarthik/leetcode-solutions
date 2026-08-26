168a9df59de35a214c34c4829aa2f60ab59c9e31
PosaKarthik <posakarthik16@gmail.com>
2026-08-26 22:56:59 +0530

    Time: 2 ms (91.93%), Space: 47.9 MB (20.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -13,6 +13,16 @@ class Solution {
         nums[i]=0;
        }
 
+        // for(int i=0;i<nums.length;i++){
+
+        //     if(nums[i] != 0){
+        //         int temp = nums[i];
+        //         nums[i]=nums[j];
+        //         nums[j]=temp;   
+        //         j++;   
+        //     }
+        // }
+
 
         
     }
